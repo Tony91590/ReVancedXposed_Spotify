3bf7c4b809ec7360d323167c8a29ea3b2103089e
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-03-28 17:52:07 +0100

    Update SpotifyHook.kt

--- app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
@@ -4,19 +4,19 @@ import android.app.Application
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 import io.github.chsbuffer.revancedxposed.BaseHook
 import io.github.chsbuffer.revancedxposed.injectHostClassLoaderToSelf
-import io.github.chsbuffer.revancedxposed.spotify.misc.UnlockPremium
+//import io.github.chsbuffer.revancedxposed.spotify.misc.UnlockPremium
 import io.github.chsbuffer.revancedxposed.spotify.misc.ads.InterceptAds
-import io.github.chsbuffer.revancedxposed.spotify.misc.privacy.SanitizeSharingLinks
-import io.github.chsbuffer.revancedxposed.spotify.misc.widgets.FixThirdPartyLaunchersWidgets
+//import io.github.chsbuffer.revancedxposed.spotify.misc.privacy.SanitizeSharingLinks
+//import io.github.chsbuffer.revancedxposed.spotify.misc.widgets.FixThirdPartyLaunchersWidgets
 
 @Suppress("UNCHECKED_CAST")
 class SpotifyHook(app: Application, lpparam: LoadPackageParam) : BaseHook(app, lpparam) {
     override val hooks = arrayOf(
-        ::Extension,
-        ::SanitizeSharingLinks,
-        ::UnlockPremium,
+        //::Extension,
+        //::SanitizeSharingLinks,
+        //::UnlockPremium,
         ::InterceptAds,
-        ::FixThirdPartyLaunchersWidgets
+        //::FixThirdPartyLaunchersWidgets
     )
 
     fun Extension() {
