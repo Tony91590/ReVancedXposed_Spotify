5c1f6b9b20fd8b4ebd07723a7c008c12711254ad
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-03-29 20:59:28 +0200

    Update build.gradle.kts

--- stub/build.gradle.kts
+++ stub/build.gradle.kts
@@ -1,11 +1,37 @@
 plugins {
-    alias(libs.plugins.android.library)
+    id("com.android.library")
+    kotlin("android")
 }
 
 android {
-    namespace = "stub"
+    namespace = "io.github.chsbuffer.revancedxposed.spotify.stub"
+    compileSdk = 34  // <--- obligatoire
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
+    implementation("de.robv.android.xposed:api:82") // Xposed API
 }
