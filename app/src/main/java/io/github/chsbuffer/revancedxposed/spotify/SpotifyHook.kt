edc70a01b6128e53803bf291238497d8bfc2e241
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-03-29 20:21:03 +0200

    Update SpotifyHook.kt

--- app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/spotify/SpotifyHook.kt
@@ -1,27 +1,3 @@
 package io.github.chsbuffer.revancedxposed.spotify
 
-import android.app.Application
-import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
-import io.github.chsbuffer.revancedxposed.BaseHook
-import io.github.chsbuffer.revancedxposed.injectHostClassLoaderToSelf
-//import io.github.chsbuffer.revancedxposed.spotify.misc.UnlockPremium
-import io.github.chsbuffer.revancedxposed.spotify.misc.ads.InterceptAds
-//import io.github.chsbuffer.revancedxposed.spotify.misc.privacy.SanitizeSharingLinks
-//import io.github.chsbuffer.revancedxposed.spotify.misc.widgets.FixThirdPartyLaunchersWidgets
-
-@Suppress("UNCHECKED_CAST")
-class SpotifyHook(app: Application, lpparam: LoadPackageParam) : BaseHook(app, lpparam) {
-    override val hooks = arrayOf(
-        //::Extension,
-        //::SanitizeSharingLinks,
-        //::UnlockPremium,
-        ::InterceptAds,
-        //::FixThirdPartyLaunchersWidgets
-    )
-
-    fun Extension() {
-        // load stubbed spotify classes
-        injectHostClassLoaderToSelf(this::class.java.classLoader!!, classLoader)
-    }
-}
-
+class SpotifyHook(val classLoader: ClassLoader)
