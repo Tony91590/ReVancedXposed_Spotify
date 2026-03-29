1b8d6fcf9e08f4075d1331b88d1e4f387b0f48fc
Tony91590 <91764010+Tony91590@users.noreply.github.com>
2026-03-29 20:27:47 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,25 +1,26 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk 34
+    namespace = "io.github.chsbuffer.revancedxposed"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "io.github.chsbuffer.revancedxposed"
-        minSdk 24
-        targetSdk 34
+        applicationId = "io.github.chsbuffer.revancedxposed"
+        minSdk = 24
+        targetSdk = 34
     }
 
     buildTypes {
         release {
-            minifyEnabled false
+            isMinifyEnabled = false
         }
     }
 }
 
 dependencies {
-    compileOnly 'de.robv.android.xposed:api:82'
-    implementation "dnsjava:dnsjava:3.5.2"
+    compileOnly("de.robv.android.xposed:api:82")
+    implementation("dnsjava:dnsjava:3.5.2")
 }
